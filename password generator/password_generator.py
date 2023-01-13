10b9fe0a804cb7410f669be6abd55a88af821bb2
jubatwain <102503462+jubatwain@users.noreply.github.com>
2023-01-13 11:47:43 +0300

    Add files via upload

---
+++ password generator/password_generator.py
@@ -0,0 +1,20 @@
+# necessary imports
+import secrets
+import string
+
+# define the alphabet
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+
+alphabet = letters + digits + special_chars
+
+# fix password length
+pwd_length = 12
+
+# generate a password string
+pwd = ''
+for i in range(pwd_length):
+  pwd += ''.join(secrets.choice(alphabet))
+
+print(pwd)
